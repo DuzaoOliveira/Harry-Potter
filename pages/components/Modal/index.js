c678b55b8e58e1af24c434bddd6c499a68a77c0c
DuzaoOliveira <edu.oliblack@hotmail.com>
2022-11-18 16:21:08 -0300

    arrumar em casa

---
+++ pages/components/Modal/index.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+import styles from '../Modal'
+
+
+
+const Modal = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Modal
--- pages/personagens/index.js
+++ pages/personagens/index.js
@@ -1,11 +1,25 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import Modal from '../components/Modal'
 import styles from './styles.module.css';
-import Head from 'next/head'
+import axios from "axios";
+import Head from 'next/head';
+
+// Modal.setAppElement('#root');
 
 function Persongens() {
 
   const [resposta, setResposta] = useState();
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [valores, setValores] = useState();
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     axios
@@ -19,6 +33,25 @@ function Persongens() {
   const handleClick = () => {
     window.location.href = "/";
   };
+
+  const escolherPersonagem = (personagem) => {
+    setResposta(true);
+    axios
+      .get(
+        `https://hp-api.herokuapp.com/api/characters/${personagem}`,
+      )
+      .then((response) => {
+        setResposta(response?.data.results);
+        setIsOpen(true);
+      })
+
+  }
+
+  const handleOpenModal = (personagem) => {
+    setPersonagemAtual(personagem);
+    setModalIsOpen(true)
+  };
+
 
   return (
 
@@ -52,17 +85,49 @@ function Persongens() {
 
       <div className={styles.boxcard}>
 
-        {resposta?.map((personagens) => {
+        {resposta?.map((personagens,) => {
           return (
-            <>
-              <img src={personagens.image}></img>
 
-              {/* <p>{personagens.name}</p> */}
-            </>
+            <><Modal>
+
+            </Modal>
+
+              <div className={styles.card} onClick={() => escolherPersonagem(personagens.name)}>
+
+
+                <div className={styles.nome}>
+                  <div className={styles.todos}>
+                    <img src={personagens.image}></img>
+                  </div>
+
+                  <p>{personagens.name}</p>
+                </div>
+              </div></>
           );
 
         })}
       </div>
+
+      {/* {valores &&
+        <Modal
+          nome={valores?.name}
+          species={valores.types && valores?.types[0]?.type?.name}
+          gender={valores.stats && valores?.stats[0].base_stat}
+          house={valores.stats && valores?.stats[1].base_stat}
+          dateOfBirth={valores.stats && valores?.stats[2].base_stat}
+          patronus={valores.stats && valores?.stats[3].base_stat}
+          wood={valores.stats && valores?.stats[4].base_stat}
+          length={valores.stats && valores?.stats[5].base_stat}
+          core={valores.stats && valores?.stats[6].base_stat}
+          ancestry={valores.stats && valores?.stats[7].base_stat}
+          actor={valores.stats && valores?.stats[8].base_stat}
+          imagem={
+            <img src={personagens.image}></img>
+          }
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+        />
+      } */}
 
     </div>
   )
